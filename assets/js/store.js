666bcb4ed0c8fa6ec1d23d351043f8900172423a
Yoann <yoann.delion@samsic.fr>
2020-04-21 11:04:49 +0200

    Redux persist: only store auth data

--- assets/js/store.js
+++ assets/js/store.js
@@ -6,8 +6,9 @@ import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
-    key: 'root',
+    key: 'authReducer',
     storage,
+    whitelist: ['authReducer']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
